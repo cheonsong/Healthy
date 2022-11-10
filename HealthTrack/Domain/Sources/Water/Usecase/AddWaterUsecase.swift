5bba49f1ccd6eab5ba3115a4f52dd3cbc6b7d6f9
cheonsong <qkrcjsthd@gmail.com>
2022-11-10 18:01:45 +0900

    [#9]Feat: WaterAddUsecase 구현

--- HealthTrack/Domain/Sources/Water/Usecase/AddWaterUsecase.swift
+++ HealthTrack/Domain/Sources/Water/Usecase/AddWaterUsecase.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+public protocol AddWaterUsecaseProtocol {
+    func excute(data: DailyWaterModel)-> Bool
+}
+
+public class AddWaterUsecase: AddWaterUsecaseProtocol {
+    
+    var repository: WaterRepositoryProtocol
+    
+    public init(repository: WaterRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    public func excute(data: DailyWaterModel) -> Bool {
+        return repository.saveTodayWater(data: data)
+    }
+}
