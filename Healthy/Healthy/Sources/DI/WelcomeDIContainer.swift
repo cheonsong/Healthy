b3898fde71f68784c8fbe5af2f1eb1fce0fcaa32
cheonsong <qkrcjsthd@gmail.com>
2022-11-18 17:12:04 +0900

    Feat: WelcomeDIContainer 구현

---
+++ Healthy/Healthy/Sources/DI/WelcomeDIContainer.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeDIContainer.swift
+//  Healthy
+//
+//  Created by cheonsong on 2022/11/18.
+//
+
+import Foundation
+import Presentation
+import UIKit
+
+final class WelcomeDIContainer {
+    init() {}
+    
+    func makeWelcomeCoordinator(navigation: UINavigationController)-> WelcomeCoordinator {
+        return WelcomeCoordinator(navigationController: navigation, dependencies: self)
+    }
+}
+
+extension WelcomeDIContainer: WelcomCoordinatorDependencies {
+    func makeWelcomeViewController(action: WelcomeViewModelAction)-> WelcomeViewContoller {
+        return WelcomeViewContoller.create(action: action)
+    }
+    
+    func makeMainCoordinator(navigaion navigation: UINavigationController)-> MainCoordinator {
+        return MainDIContainer().makeMainCoordinator(navigationController: navigation)
+    }
+}
