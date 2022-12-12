5826b7ab0dafe5ddf7d3d9da35d64b0a6965b186
박천송 <qkrcjsthd@gmail.com>
2022-12-12 16:48:56 +0900

    Fix: Scripts 수정

--- Tuist/ProjectDescriptionHelpers/Scripts.swift
+++ Tuist/ProjectDescriptionHelpers/Scripts.swift
@@ -8,6 +8,6 @@
 import ProjectDescription
 
 extension ProjectDescription.TargetScript {
-//    public static let localize = TargetScript.pre(script: "python3.10 ../../Scripts/localize.py", name: "Localize", basedOnDependencyAnalysis: true, runForInstallBuildsOnly: true)
-    public static let localize = TargetScript.pre(path: .relativeToRoot("Scripts/Localize.sh"), name: "Localize")
+    public static let localize = TargetScript.pre(script: "python3.10 ../../Scripts/localize.py", name: "Localize", basedOnDependencyAnalysis: true, runForInstallBuildsOnly: true)
+    //public static let localize = TargetScript.pre(path: .relativeToRoot("Scripts/Localize.sh"), name: "Localize")
 }
