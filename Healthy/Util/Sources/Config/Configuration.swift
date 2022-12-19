01f11eac538c7faa38c66a4e55f59e7754cd067e
박천송 <qkrcjsthd@gmail.com>
2022-12-19 14:00:37 +0900

    Feat: Util XCConfiguration 매니저 추가

---
+++ Healthy/Util/Sources/Config/Configuration.swift
@@ -0,0 +1,30 @@
+//
+//  Configuration.swift
+//  Util
+//
+//  Created by cheonsong on 2022/12/19.
+//
+
+import Foundation
+
+public enum ConfigureEnum {
+    case slackChannel
+    case slackBotToken
+    
+    var key: String {
+        switch self {
+        case .slackChannel:
+            return "SlackChannel"
+        case .slackBotToken:
+            return "SlackBotUserToken"
+        }
+    }
+}
+
+public struct Configure {
+    
+    public static func getConfig(_ config: ConfigureEnum)-> String {
+        return Bundle.main.object(forInfoDictionaryKey: config.key) as? String ?? "Invalid Key"
+    }
+    
+}
