bf89c3cbbb5ff4d33a190c45fcf8b275f1c611af
박천송 <qkrcjsthd@gmail.com>
2022-12-28 16:11:13 +0900

    Feat: Color 추가

--- WidgetExtension/Color+Extension.swift
+++ WidgetExtension/Color+Extension.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
+import DesignSystem
+
+extension Color {
+    static let b1: Color = Color(UIColor.b1)
+    static let b2: Color = Color(UIColor.b2)
+    static let b3: Color = Color(UIColor.b3)
+}
+
+extension Image {
+    static let water: Image = Image("icoWater")
+    static let steps: Image = Image("icoSteps")
+    static let calories: Image = Image("icoCalories")
+    static let sleep: Image = Image("icoSleep")
+}
